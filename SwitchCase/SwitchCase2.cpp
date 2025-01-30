93078d68074760ea83fe8e7bb3261322d4c9f5ae
Gyan-max <gyanranjan4427@gmail.com>
2025-01-30 11:24:10 +0530

    SwitchCase Update

---
+++ SwitchCase/SwitchCase2.cpp
@@ -0,0 +1,25 @@
+// Take a day number and print corresponding day of the week.
+// for 1= Monday, for 2 = Tuesday and so on for 7 =sunday
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int day;
+    cout << "Enter a number: "; cin>>day;
+    switch(day){
+        case 1: cout<<"Monday";
+        break;
+        case 2: cout<<"Tuesday";
+        break;
+        case 3: cout<<"Wednesday";
+        break;
+        case 4: cout<<"Thursday";
+        break;
+        case 5: cout<<"Friday";
+        break;
+        case 6: cout<<"Saturday";
+        break;
+        case 7: cout<<"Sunday";
+        break;
+    }
+    return 0;
+}
