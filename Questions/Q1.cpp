771af27bbce1a7eb061a95b5c18c0227c4970dc5
Gyan-max <gyanranjan4427@gmail.com>
2025-01-31 12:18:10 +0530

    Added some practice Questions

---
+++ Questions/Q1.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+//Program to calculate area of a square
+int main() {
+    int side;
+    cout<<"Enter side: "; cin>>side;
+    int area = side*side;
+    cout<<"Area of the square is: "<< area;
+    return 0;
+}
---
+++ Questions/Q2.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+//Program to return minimum of two numbers
+int main() {
+    int x, y;
+    cout<<"Enter first number : "; cin >>x;
+    cout<<"Enter second number : "; cin>>y;
+    if (x<y){
+        cout<<x;
+    }else{
+        cout<<y;
+    }
+    return 0;
+
+}
